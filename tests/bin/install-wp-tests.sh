0f367b5d2fa2d9e483e55ac6765b1a4bceff181e
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2026-02-03 18:30:27 -0300

    Now install-wp-tests follows redirects

--- tests/bin/install-wp-tests.sh
+++ tests/bin/install-wp-tests.sh
@@ -25,7 +25,7 @@ WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
 
 download() {
 	if [ $(which curl) ]; then
-		curl -s "$1" > "$2";
+		curl -sL "$1" > "$2";
 	elif [ $(which wget) ]; then
 		wget -nv -O "$2" "$1"
 	fi
