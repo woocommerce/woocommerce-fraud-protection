47ae66f0336e1540301e1e35d26eabb96de6f5b3
Luiz Reis <luiz.reis@automattic.com>
2026-04-16 14:50:15 -0300

    Fraud Protection: Fix asset URLs when plugin file is outside WP_CONTENT_DIR
    When the plugin file is loaded from outside WP_CONTENT_DIR (e.g. via a
    symlink), PHP resolves __FILE__ to the real path, so plugins_url() falls
    through its mu-plugin/plugin checks and generates URLs that don't point
    at the actual mu-plugins location. Add a plugins_url filter in the loader
    to rewrite the resolved path back to the mu-plugins URL.

--- woocommerce-fraud-protection-loader.php
+++ woocommerce-fraud-protection-loader.php
@@ -10,4 +10,19 @@
 
 declare( strict_types = 1 );
 
+/**
+ * Filter plugins_url for when __FILE__ is outside of WP_CONTENT_DIR.
+ *
+ * @param string $url The complete URL to the plugins directory including scheme and path.
+ * @return string Filtered URL.
+ */
+function woocommerce_fraud_protection_symlinked_plugins_url( $url ) {
+	return preg_replace(
+		'#((?<!/)/[^/]+)*/wp-content/plugins/wordpress/plugins/woocommerce-fraud-protection/([^/]+)/?#',
+		'/wp-content/mu-plugins/woocommerce-fraud-protection/',
+		$url
+	);
+}
+add_filter( 'plugins_url', 'woocommerce_fraud_protection_symlinked_plugins_url', 0, 1 );
+
 require_once WPMU_PLUGIN_DIR . '/woocommerce-fraud-protection/woocommerce-fraud-protection.php';
