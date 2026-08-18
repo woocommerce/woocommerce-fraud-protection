d405ab6cd775fb44b6a6ca2c913b46b268d43953
Luiz Reis <luiz.reis@automattic.com>
2026-08-18 16:09:37 -0300

    Fraud Protection: Bump version to 0.1.8

--- woocommerce-fraud-protection.php
+++ woocommerce-fraud-protection.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WooCommerce Fraud Protection
  * Description: A plugin to protect WooCommerce from fraud.
- * Version: 0.1.7
+ * Version: 0.1.8
  * Author: Automattic
  * Requires Plugins: woocommerce
  * Requires PHP: 8.1
