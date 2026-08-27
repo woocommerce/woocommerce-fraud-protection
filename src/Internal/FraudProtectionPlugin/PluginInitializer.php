00107f5132835dc6421fedb9d9fe0b427c8051aa
Luiz Reis <luiz.reis@automattic.com>
2026-08-27 10:45:09 -0300

    Fraud Protection: Bump version to 0.2.0

--- src/Internal/FraudProtectionPlugin/PluginInitializer.php
+++ src/Internal/FraudProtectionPlugin/PluginInitializer.php
@@ -42,7 +42,7 @@ class PluginInitializer {
 	 * @return void
 	 */
 	public static function run( string $plugin_file ): void {
-		define( 'WC_FRAUD_PROTECTION_VERSION', '0.1.9' );
+		define( 'WC_FRAUD_PROTECTION_VERSION', '0.2.0' );
 		define( 'WC_FRAUD_PROTECTION_PLUGIN_DIR', dirname( $plugin_file ) );
 		if ( ! defined( 'WC_FRAUD_PROTECTION_PLUGIN_URL' ) ) {
 			define( 'WC_FRAUD_PROTECTION_PLUGIN_URL', plugin_dir_url( $plugin_file ) );
