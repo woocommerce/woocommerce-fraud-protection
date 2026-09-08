012dcaea51dc1db23404e7d32e8201aa41cea3b5
Luiz Reis <luiz.reis@automattic.com>
2026-09-08 16:07:19 -0300

    Add checkout attempts SPA route

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const WooCommerceDependencyExtractionWebpackPlugin = require( '@woocommerce/dependency-extraction-webpack-plugin' );
 
 module.exports = {
 	...defaultConfig,
+	plugins: [
+		...defaultConfig.plugins.filter(
+			( plugin ) =>
+				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
+		),
+		new WooCommerceDependencyExtractionWebpackPlugin(),
+	],
 	entry: {
 		'admin-settings': './client/admin-settings/index.tsx',
 	},
